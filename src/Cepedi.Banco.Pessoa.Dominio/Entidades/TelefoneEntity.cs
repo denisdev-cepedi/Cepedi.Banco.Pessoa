e1b309f18b5adcb670d176bf2468a54b88068a40
Marcelo da Silva Cruz <mcruz@uesc.br>
2024-04-21 12:21:00 -0300

    Implementando EntityTypeConfiguration e Entidade - Telefone

---
+++ src/Cepedi.Banco.Pessoa.Dominio/Entidades/TelefoneEntity.cs
@@ -0,0 +1,14 @@
+namespace Cepedi.Banco.Pessoa.Dominio.Entidades;
+
+public class TelefoneEntityEntity
+{
+    public int Id { get; set; }
+    public string CodPais { get; set; } = default!;
+    public string Ddd { get; set; } = default!;
+    public string Numero { get; set; } = default!;
+    public string Tipo { get; set; } = default!;
+    public bool Principal { get; set; } = default!;  
+    public int IdPessoa { get; set; }
+
+    public PessoaEntity Pessoa { get; set; } = default!;
+}
