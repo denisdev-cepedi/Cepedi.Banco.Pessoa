fe5e0dd552b9cd10de861b91d2cde4be0c73bcfb
ezeklobo <eloliveira.cic@uesc.br>
2024-04-21 12:13:22 -0300

    created entity Telefone

---
+++ src/Cepedi.Banco.Pessoa.Dominio/Entidades/TelefoneEntity.cs
@@ -0,0 +1,18 @@
+using Cepedi.Banco.Pessoa.Compartilhado.Requests;
+
+namespace Cepedi.Banco.Pessoa.Dominio.Entidades
+{
+    public class TelefoneEntity
+    {
+        public int Id { get; set; }
+        public string CodPais { get; set; } = default!;
+        public string Ddd { get; set; } = default!;
+        public string Numero { get; set; } = default!;
+        public string Tipo { get; set; } = default!;
+        public bool Principal { get; set; }
+        public int IdPessoa { get; set; }
+
+        public PessoaEntity Pessoa { get; set; } = default!;
+
+    }
+}
