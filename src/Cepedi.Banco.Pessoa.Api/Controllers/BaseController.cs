8431f7e254609e20d819ef2a28945e89defd8940
Breno Rios <breno@brenorios.com.br>
2024-04-29 19:54:09 -0300

    Corrigida falha quando lançado erro genérico

--- src/Cepedi.Banco.Pessoa.Api/Controllers/BaseController.cs
+++ src/Cepedi.Banco.Pessoa.Api/Controllers/BaseController.cs
@@ -37,7 +37,7 @@ public class BaseController : ControllerBase
 
     private ResultadoErro FormatErrorMessage(ResultadoErro responseErro, IEnumerable<string>? errors = null)
     {
-        if (errors == null)
+        if (errors is not null)
         {
             responseErro.Descricao = $"{responseErro.Descricao} : {string.Join("; ", errors!)}";
         }
