8b7cd55709ee75a8f703011556b22e15f7d59ff8
Alan Carlos <alancarlospereira.ti@gmail.com>
2024-05-20 17:02:12 -0300

    adicionando interface

---
+++ src/Cepedi.Banco.Pessoa.Dominio/Servicos/ICache.cs
@@ -0,0 +1,6 @@
+namespace Cepedi.Banco.Pessoa.Dominio.Services;
+public interface ICache<T>
+{
+    Task<T> ObterAsync(string chave);
+    Task SalvarAsync(string chave, T objeto, int tempoExpiracao = 10);
+}
