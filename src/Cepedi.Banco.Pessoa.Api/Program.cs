7d8be3d71a636daf2640b0cc3ac5ae3d1b48883c
Marcelo da Silva Cruz <mcruz@uesc.br>
2024-05-22 18:11:17 -0300

    logging-implementation

--- src/Cepedi.Banco.Pessoa.Api/Program.cs
+++ src/Cepedi.Banco.Pessoa.Api/Program.cs
@@ -1,22 +1,55 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using Cepedi.Banco.Pessoa.IoC;
 using Cepedi.Banco.Pessoa.Api;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 builder.Services.ConfigureAppDependencies(builder.Configuration);
 
-builder.Host.UseSerilog((context, configuration) =>
+// Configure Serilog
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}"
+    })
+    .CreateLogger();
+
+builder.Host.UseSerilog(logger);
+
+// Keycloak configuration
+var keycloakConfig = builder.Configuration.GetSection("Keycloak");
+builder.Services.AddAuthentication(options =>
 {
-    configuration.ReadFrom.Configuration(context.Configuration);
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = $"{keycloakConfig["auth-server-url"]}realms/{keycloakConfig["realm"]}";
+    options.RequireHttpsMetadata = false;
+    options.Audience = keycloakConfig["resource"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidAudience = keycloakConfig["resource"],
+        ValidIssuer = $"{keycloakConfig["auth-server-url"]}realms/{keycloakConfig["realm"]}"
+    };
 });
 
 var app = builder.Build();
@@ -32,6 +65,7 @@ if (app.Environment.IsDevelopment())
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 
+app.UseAuthentication();  // Add this line to enable authentication middleware
 app.UseAuthorization();
 
 app.MapControllers();
@@ -39,4 +73,3 @@ app.MapControllers();
 app.Map("/", () => Results.Redirect("/swagger"));
 
 app.Run();
-
