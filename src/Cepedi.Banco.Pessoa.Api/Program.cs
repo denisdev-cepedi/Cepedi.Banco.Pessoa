25f85ff1bdfc0823ceb4a0a6ec0e9fa59855be80
Marcelo da Silva Cruz <mcruz@uesc.br>
2024-05-22 18:00:02 -0300

    keycloak-integration

--- src/Cepedi.Banco.Pessoa.Api/Program.cs
+++ src/Cepedi.Banco.Pessoa.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Cepedi.Banco.Pessoa.IoC;
 using Cepedi.Banco.Pessoa.Api;
@@ -5,18 +7,39 @@ using Cepedi.Banco.Pessoa.Api;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 builder.Services.ConfigureAppDependencies(builder.Configuration);
 
+// Configure Serilog
 builder.Host.UseSerilog((context, configuration) =>
 {
     configuration.ReadFrom.Configuration(context.Configuration);
+});
+
+// Keycloak configuration
+var keycloakConfig = builder.Configuration.GetSection("Keycloak");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = $"{keycloakConfig["auth-server-url"]}realms/{keycloakConfig["realm"]}";
+    options.RequireHttpsMetadata = false;
+    options.Audience = keycloakConfig["resource"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidAudience = keycloakConfig["resource"],
+        ValidIssuer = $"{keycloakConfig["auth-server-url"]}realms/{keycloakConfig["realm"]}"
+    };
 });
 
 var app = builder.Build();
@@ -32,6 +55,7 @@ if (app.Environment.IsDevelopment())
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 
+app.UseAuthentication();  // Add this line to enable authentication middleware
 app.UseAuthorization();
 
 app.MapControllers();
@@ -39,4 +63,3 @@ app.MapControllers();
 app.Map("/", () => Results.Redirect("/swagger"));
 
 app.Run();
-
