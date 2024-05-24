61d2afee38e225d8a5e0e7b18b4f363661be1808
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-05-24 03:06:16 -0300

    Otimização ObterPessoa: Retornar informação de endereço e numero princiapal

--- src/Cepedi.Banco.Pessoa.Compartilhado/Responses/Pessoa/ObterPessoaResponse.cs
+++ src/Cepedi.Banco.Pessoa.Compartilhado/Responses/Pessoa/ObterPessoaResponse.cs
@@ -1,13 +1,17 @@
-namespace Cepedi.Banco.Pessoa.Compartilhado.Responses;
-
-public class ObterPessoaResponse
+namespace Cepedi.Banco.Pessoa.Compartilhado.Responses
 {
-    public int Id { get; set; }
-    public string Nome { get; set; } = default!;
-    public string Email { get; set; } = default!;
-    public DateTimeOffset DataNascimento { get; set; }
-    public string Cpf { get; set; } = default!;
-    public string Genero { get; set; } = default!;
-    public string EstadoCivil { get; set; } = default!;
-    public string Nacionalidade { get; set; } = default!;
+    public class ObterPessoaResponse
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public DateTimeOffset DataNascimento { get; set; }
+        public string Cpf { get; set; } = default!;
+        public string Genero { get; set; } = default!;
+        public string EstadoCivil { get; set; } = default!;
+        public string Nacionalidade { get; set; } = default!;
+
+        public CadastrarTelefoneResponse? TelefonePrincipal { get; set; }
+        public CadastrarEnderecoResponse? EnderecoPrincipal { get; set; }
+    }
 }
--- src/Cepedi.Banco.Pessoa.Dominio/Handlers/Pessoa/ObterPessoaRequestHandler.cs
+++ src/Cepedi.Banco.Pessoa.Dominio/Handlers/Pessoa/ObterPessoaRequestHandler.cs
@@ -1,20 +1,31 @@
-using Cepedi.Banco.Pessoa.Compartilhado.Responses;
-using Cepedi.Banco.Pessoa.Compartilhado.Requests;
+using Cepedi.Banco.Pessoa.Dominio.Entidades;
 using Cepedi.Banco.Pessoa.Dominio.Repository;
+using Cepedi.Banco.Pessoa.Compartilhado.Responses; 
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OperationResult;
+using System.Threading;
+using System.Threading.Tasks;
+using Cepedi.Banco.Pessoa.Compartilhado.Requests;
 
 namespace Cepedi.Banco.Pessoa.Dominio.Handlers
 {
     public class ObterPessoaRequestHandler : IRequestHandler<ObterPessoaRequest, Result<ObterPessoaResponse>>
     {
         private readonly IPessoaRepository _pessoaRepository;
+        private readonly ITelefoneRepository _telefoneRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
         private readonly ILogger<ObterPessoaRequestHandler> _logger;
 
-        public ObterPessoaRequestHandler(IPessoaRepository pessoaRepository, ILogger<ObterPessoaRequestHandler> logger)
+        public ObterPessoaRequestHandler(
+            IPessoaRepository pessoaRepository,
+            ITelefoneRepository telefoneRepository,
+            IEnderecoRepository enderecoRepository,
+            ILogger<ObterPessoaRequestHandler> logger)
         {
             _pessoaRepository = pessoaRepository;
+            _telefoneRepository = telefoneRepository;
+            _enderecoRepository = enderecoRepository;
             _logger = logger;
         }
 
@@ -23,9 +34,17 @@ namespace Cepedi.Banco.Pessoa.Dominio.Handlers
             var pessoa = await _pessoaRepository.ObterPessoaAsync(request.PessoaId);
             if (pessoa is null)
             {
-                _logger.LogError("Pessoa não encontrada.");
+                _logger.LogError("Pessoa não encontrada.");
                 return Result.Error<ObterPessoaResponse>(new Compartilhado.Exceptions.PessoaNaoEncontradaExcecao());
             }
+
+            
+            var telefonePrincipal = await _telefoneRepository.ObterTelefonePrincipalAsync(pessoa.Id);
+            var enderecoPrincipal = await _enderecoRepository.ObterEnderecoPrincipalAsync(pessoa.Id);
+
+            
+            var telefonePrincipalResponse = MapToCadastrarTelefoneResponse(telefonePrincipal);
+            var enderecoPrincipalResponse = MapToCadastrarEnderecoResponse(enderecoPrincipal);
 
             var pessoaResponse = new ObterPessoaResponse
             {
@@ -36,7 +55,9 @@ namespace Cepedi.Banco.Pessoa.Dominio.Handlers
                 Cpf = pessoa.Cpf,
                 Genero = pessoa.Genero,
                 EstadoCivil = pessoa.EstadoCivil,
-                Nacionalidade = pessoa.Nacionalidade
+                Nacionalidade = pessoa.Nacionalidade,
+                TelefonePrincipal = telefonePrincipalResponse,
+                EnderecoPrincipal = enderecoPrincipalResponse
             };
 
             _logger.LogInformation("Retornando Pessoa encontrada.");
@@ -44,5 +65,34 @@ namespace Cepedi.Banco.Pessoa.Dominio.Handlers
             return Result.Success(pessoaResponse);
         }
 
+        private CadastrarTelefoneResponse MapToCadastrarTelefoneResponse(TelefoneEntity telefone)
+        {
+            
+            return new CadastrarTelefoneResponse
+            {
+                CodPais = telefone.CodPais,
+                Ddd = telefone.Ddd,
+                Numero = telefone.Numero,
+                Tipo = telefone.Tipo,
+                Principal = telefone.Principal
+            };
+        }
+
+        private CadastrarEnderecoResponse MapToCadastrarEnderecoResponse(EnderecoEntity endereco)
+        {
+            
+            return new CadastrarEnderecoResponse
+            {
+                Cep = endereco.Cep,
+                Logradouro = endereco.Logradouro,
+                Complemento = endereco.Complemento,
+                Bairro = endereco.Bairro,
+                Cidade = endereco.Cidade,
+                Uf = endereco.Uf,
+                Pais = endereco.Pais,
+                Numero = endereco.Numero,
+                Principal = endereco.Principal
+            };
+        }
     }
 }
